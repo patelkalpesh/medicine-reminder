ea7bc60b3f2b1838325597823e0539d9cc7a5a5b
KalpeshPatel-mobilearts <kalpeshpatel@mobilearts.com>
2026-06-27 13:12:07 +0530

    v5.2: Force Upload button to fix sync conflicts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'med-reminder-v5.1';
+const CACHE_NAME = 'med-reminder-v5.2';
 const ASSETS = ['/', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'icon.svg'];
 
 self.addEventListener('install', e => {
